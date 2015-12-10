9dac1f26779d9003bfb8439119c565d696d209b4
Chris Townsend <ctownsen357@gmail.com>
2015-12-09 22:44:00 -0500

    First commit

---
+++ readWriteBinary.go
@@ -0,0 +1,55 @@
+package main
+import (
+        "fmt"
+        "os"
+		"io"
+        "encoding/binary"
+        )
+
+
+//I was needing to pack 3 int in the packed binary formt of iih for a Python struct
+//this ends up packing into the appropriate packed bytes of 4,4,2
+type binData struct {
+        EventID int32
+        WindGridID int32
+        WindSpeedX10 int16
+}
+
+
+func writeBinary(){
+        f, err := os.Create("tst.bin")
+
+        if err != nil {
+                panic(err)
+        }
+
+        defer f.Close()
+		
+		//originally I used a byte array and packed each variable in but the structure works just fine
+		bd := binData{EventID:4, WindGridID:1000, WindSpeedX10:102}		
+
+		binary.Write(f, binary.LittleEndian, &bd)	
+}
+
+func readBinary(){
+	f, err := os.Open("tst.bin")
+    if err != nil {
+            panic(err)
+    }
+	defer f.Close()
+	
+	for {
+		bd := binData{}
+		err = binary.Read(f, binary.LittleEndian, &bd)
+		
+		if err == io.EOF{
+			break
+		}
+		fmt.Println(bd.EventID, bd.WindGridID, bd.WindSpeedX10)
+	}	
+}
+func main() {
+	writeBinary()
+	readBinary()
+}
+
